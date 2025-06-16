eba5ee555986f3de2b75c046dd7ef28b5bf65ce3
Abdullah Ajmal <abdullahajmal74@gmail.com>
2025-06-16 11:14:22 +0000

    Building our Intermediate table to filter out Test Accounts

---
+++ payment_analytics_wh/models/intermediate/int_payments_filtered_accounts.sql
@@ -0,0 +1,23 @@
+with
+
+payments as (
+
+   select * from {{ ref('stg_ajar_oltp__payments') }}
+
+)
+
+,accounts as (
+    select * from {{ ref('stg_ajar_oltp__accounts') }}
+)
+
+,payments_original_accounts as (
+
+   select
+      p.* 
+      from payments p
+        join accounts a on p.account_id = p.account_id
+    where kyc_status <> 'Rejected'
+
+)
+
+select * from payments_original_accounts
